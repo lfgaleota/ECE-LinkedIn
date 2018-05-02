a04a1ba3a120413f456b25253d7e0c60857c81a4
PortableFrancis\Marine <marine.foucambert@edu.ece.fr>
2018-05-02 13:31:09 +0200

    Amelioration affichage amis

--- resources/views/app/inc/users/list.blade.php
+++ resources/views/app/inc/users/list.blade.php
@@ -1,12 +1,18 @@
 @foreach( $users as $user )
+<?php $relation=' ';?>
+	
 	@if($user->hasFriendOf())
-	@if($user->isFriendOf)
-	is your friend
-	@else
-	is not your friend
+		@if($user->isFriendOf)
+		<?php $relation=' (Friend)';?>
+
+		@else
+		<?php $relation=' ';?>
+
+		@endif
+
 	@endif
-	@endif
-    <li><a href="{{ route( 'user.profile', [ 'username' => $user->username ] ) }}">{{ $user->getName() }}</a></li>
+
+    <li><a href="{{ route( 'user.profile', [ 'username' => $user->username ] ) }}">{{ $user->getName()}} {{$relation}} </a></li>
 @endforeach
 
 {{ method_exists( $users, 'links' ) ? $users->links() : '' }}
