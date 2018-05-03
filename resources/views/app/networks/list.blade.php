2448d10857b1538e54de825639a6d2a1a1b5ea26
Rim <26852077+rz160211@users.noreply.github.com>
2018-05-04 01:30:01 +0200

    debut page network

--- resources/views/app/networks/list.blade.php
+++ resources/views/app/networks/list.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    @include( 'app.inc.users.list', [ 'users' => $networkmembers ] )
+
+    <div class="grid-container">
+    <div class="grid-x grid-padding-x">
+
+    <div class="large-12 cell">
+      <div class="callout">
+        <h3>Network</h3>
+
+      @include( 'app.inc.users.list', [ 'users' => $networkmembers ] )
+
+    </div>
+  </div>
+  </div>
+</div>
+
 @endsection
