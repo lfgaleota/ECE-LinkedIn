5d5136419eca9b782720e2397b1d0aed77e5a68d
Oui <marine.foucambert@edu.ece.fr>
2018-05-03 19:22:34 +0200

    Debut notifications

---
+++ app/Notifications/FriendRequest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class FriendRequest extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct( User $requester, User $invited, $accept )
+    {
+        $this->requester = $requester;
+        $this->invited = $invited;
+        $this->accept = $accept;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toDatabase($notifiable)
+    {
+        return $this->toArray($notifiable);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+       return [
+            'requester' => $this->requester->user_id,
+            'invited' => $this->invited->user_id,
+            'accept' => $this->accept,
+        ];
+    }
+}
---
+++ database/migrations/2018_05_03_154402_create_notifications_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $length = Illuminate\Database\Schema\Builder::$defaultStringLength;
+        Schema::defaultStringLength(191);
+        Schema::dropIfExists('notifications');
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('type');
+            $table->morphs('notifiable');
+            $table->text('data');
+            $table->timestamp('read_at')->nullable();
+            $table->timestamps();
+        });
+        Schema::defaultStringLength($length);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
