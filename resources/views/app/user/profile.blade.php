230415bb385b24a1ab8ab34828337313163d7627
Rim <26852077+rz160211@users.noreply.github.com>
2018-05-03 18:32:34 +0200

    design page profil

--- resources/views/app/user/profile.blade.php
+++ resources/views/app/user/profile.blade.php
@@ -1,8 +1,112 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{ $user->getName() }}</h1>
-    <h2>{{ $user->title or 'No title' }}</h2>
+
+  <div class="grid-container">
+
+  <div class="grid-x grid-padding-x">
+
+  <div class="large-8 medium-8 cell">
+    <div class="callout">
+      <h1>Profile</h1>
+      <h1>{{ $user->getName() }}</h1>
+      <h2>{{ $user->title or 'No title' }}</h2>
+      <h3>pouet cover pouet pp </h3>
+    </div>
+  </div>
+
+  <div class="large-4 medium-4 cell">
+    <div class="callout">
+      <h5>Your network</h5>
+      <p>Network</p>
+      @include( 'app.inc.users.list', ['users' => $user->getNetworkMembers()])
+    </div>
+  </div>
+
+  <div class="large-8 medium-8 cell">
+    <div class="callout">
+      <h5>Experience:</h5>
+      <!-- Grid -->
+      <div class="grid-x grid-padding-x">
+        <div class="large-12 cell">
+          <div class="primary callout">
+            <p><strong>ECE Paris</strong> Diplome coloriage.</p>
+          </div>
+        </div>
+      </div>
+      <div class="grid-x grid-padding-x">
+        <div class="large-6 medium-6 cell">
+          <div class="primary callout">
+            <p>kebabiste</p>
+          </div>
+        </div>
+        <div class="large-6 medium-6 cell">
+          <div class="primary callout">
+            <p>6 ans</p>
+          </div>
+        </div>
+      </div>
+      <div class="grid-x grid-padding-x">
+        <div class="large-4 medium-4 small-4 cell">
+          <div class="primary callout">
+            <p>oui</p>
+          </div>
+        </div>
+        <div class="large-4 medium-4 small-4 cell">
+          <div class="primary callout">
+            <p>des fois</p>
+          </div>
+        </div>
+        <div class="large-4 medium-4 small-4 cell">
+          <div class="primary callout">
+            <p>non</p>
+          </div>
+        </div>
+      </div>
+    </div>
+          <hr/>
+  </div>
+
+  <div class="large-8 medium-8 cell">
+    <div class="callout">
+      <h5>Education:</h5>
+      <!-- Grid -->
+      <div class="grid-x grid-padding-x">
+        <div class="large-12 cell">
+          <div class="primary callout">
+            <p><strong>ECE Paris</strong> Diplome coloriage.</p>
+          </div>
+        </div>
+      </div>
+      <div class="grid-x grid-padding-x">
+        <div class="large-6 medium-6 cell">
+          <div class="primary callout">
+            <p>kebabiste</p>
+          </div>
+        </div>
+        <div class="large-6 medium-6 cell">
+          <div class="primary callout">
+            <p>6 ans</p>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+          <hr />
+  </div>
+
+    <div class="large-4 medium-4 cell">
+
+    <h5>Des boutons a utiliser maybe lol:</h5><br/>
+    <p><a href="#" class="button">prout</a><br/>
+    <a href="#" class="success button">pouet</a><br/>
+    <a href="#" class="alert button">heh</a><br/>
+    <a href="#" class="secondary button">oui</a></p>
+  </div>
+
+</div>
+</div>
+
     @guest
         <a href="{{ route( 'index' ) }}">Connect to interact</a>
     @else
@@ -10,8 +114,8 @@
             You are viewing your own profile
             <p>{{ $user->cover_id or '' }}</p>
             <p>{{ $user->photo_id or '' }}</p>
-            
-            <p>Network</p>
+
+            <p>Network</p> <!-- if auth::user blabla or -->
             @include( 'app.inc.users.list', ['users' => $user->getNetworkMembers()])
           <p>Informations</p>
          <p><button class="button" data-toggle="profileEditModal">Edit le profil</button></p>
@@ -23,13 +127,13 @@
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-          
-            
+
+
         @else
-            
+
             @if( Auth::user()->isInNetwork( $user ) )
                 @include( 'app.inc.buttons.remove.network', [ 'username' => $user->username ] )
-            
+
             @else
                 @include( 'app.inc.buttons.add.network', [ 'username' => $user->username ] )
             @endif
@@ -44,7 +148,7 @@
             @elseif (Auth::user()->wasAskedFriend( $user ) )
                 @include( 'app.inc.buttons.add.request', [ 'username' => $user->username ] )
                 @include( 'app.inc.buttons.remove.request', [ 'username' => $user->username ] )
-           
+
             @else
                 @include( 'app.inc.buttons.add.friend', [ 'username' => $user->username ] )
             @endif
