6ff63118e689fed35b97e78ef9e4e5b51136753f
Rim <26852077+rz160211@users.noreply.github.com>
2018-05-04 20:22:53 +0200

    Forme pour Infos 2.0

--- resources/views/app/inc/forms/edit-experience.blade.php
+++ resources/views/app/inc/forms/edit-experience.blade.php
@@ -1,6 +1,16 @@
 <div class="edit-experience">
     {!!  Form::model($user, ['route' => ['user.update', $user->username], 'files' => true]) !!}
 
+          <div>
+              {{ Form::label('titre', 'Title', ['class' => ($errors->has('name') ? 'is-invalid-label' : '')]) }}
+              {{ Form::text('titre', null, ['required' => true, 'class' => ($errors->has('name') ? 'is-invalid-input' : '')]) }}
+              @if ($errors->has('titre'))
+                  <span class="form-error is-visible">
+                      {{ $errors->first('titre') }}
+                  </span>
+              @endif
+          </div>
+
             <div>
                 {{ Form::label('company', 'Company', ['class' => ($errors->has('name') ? 'is-invalid-label' : '')]) }}
                 {{ Form::text('company', null, ['required' => true, 'class' => ($errors->has('name') ? 'is-invalid-input' : '')]) }}
@@ -22,16 +32,6 @@
             </div>
 
             <div>
-                {{ Form::label('titre', 'Title', ['class' => ($errors->has('name') ? 'is-invalid-label' : '')]) }}
-                {{ Form::text('titre', null, ['required' => true, 'class' => ($errors->has('name') ? 'is-invalid-input' : '')]) }}
-                @if ($errors->has('titre'))
-                    <span class="form-error is-visible">
-                        {{ $errors->first('titre') }}
-                    </span>
-                @endif
-            </div>
-
-            <div>
                 {{ Form::label('start', 'From', ['class' => ($errors->has('surname') ? 'is-invalid-label' : '')]) }}
                 {{ Form::date('start', null, ['required' => true, 'class' => ($errors->has('birth_date') ? 'is-invalid-input' : '')]) }}
                 @if ($errors->has('birth_date'))
@@ -41,7 +41,16 @@
                 @endif
             </div>
 
-
+            <div>
+                {{ Form::label('end', 'To', ['class' => ($errors->has('surname') ? 'is-invalid-label' : '')]) }}
+                {{ Form::date('end', null, ['required' => true, 'class' => ($errors->has('birth_date') ? 'is-invalid-input' : '')]) }}
+                @if ($errors->has('birth_date'))
+                    <span class="form-error is-visible">
+                        {{ $errors->first('birth_date') }}
+                    </span>
+                @endif
+                <input id="currentWork" type="checkbox"><label for="checkbox1">I currently work here</label>
+            </div>
 
             <div>
                 {{ Form::submit('Save', ['class' => 'button expanded']) }}
