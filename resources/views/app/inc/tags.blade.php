b848d595da991f57b7a86f8e6b0c46a1174ea211
louisfelix90 <louisfelix@galeota.fr>
2018-05-04 17:23:25 +0200

    Sélection des photos de profil et covers

---
+++ resources/views/app/inc/tags.blade.php
@@ -0,0 +1,13 @@
+<script data-src="{{ asset( 'tags/post-form.tag' ) }}" type="riot/tag"></script>
+<script data-src="{{ asset( 'tags/image-form.tag' ) }}" type="riot/tag"></script>
+<script data-src="{{ asset( 'tags/video-form.tag' ) }}" type="riot/tag"></script>
+<script data-src="{{ asset( 'tags/tag-selector.tag' ) }}" type="riot/tag"></script>
+<script data-src="{{ asset( 'tags/tag-infinite-scroller.tag' ) }}" type="riot/tag"></script>
+<script data-src="{{ asset( 'tags/friend-renderer.tag' ) }}" type="riot/tag"></script>
+<script data-src="{{ asset( 'tags/image-renderer.tag' ) }}" type="riot/tag"></script>
+<script data-src="{{ asset( 'tags/video-renderer.tag' ) }}" type="riot/tag"></script>
+<script data-src="{{ asset( 'tags/post-renderer.tag' ) }}" type="riot/tag"></script>
+<script data-src="{{ asset( 'tags/like-button.tag' ) }}" type="riot/tag"></script>
+<script data-src="{{ asset( 'tags/spinner.tag' ) }}" type="riot/tag"></script>
+<script data-src="{{ asset( 'tags/image-popup.tag' ) }}" type="riot/tag"></script>
+<script data-src="{{ asset( 'tags/video-popup.tag' ) }}" type="riot/tag"></script>
