352bb3516c6d6fd6c2da0dfe045983245471c1d2
horgeon <me@horgeon.fr>
2018-05-05 23:05:04 +0200

    Correction

--- database/migrations/2018_05_03_154402_create_notifications_table_real.php
+++ database/migrations/2018_05_03_154402_create_notifications_table_real.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateNotificationsTable extends Migration
+class CreateNotificationsTableReal extends Migration
 {
     /**
      * Run the migrations.
